60f88842c70b2348391914e009eb5a957b9cdc72
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-11-30 15:55:42 -0600

    jk the backbutton will always go back to the home page

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,25 +1,19 @@
 'use client';
 
-import { useState } from 'react';
-import { ChevronLeft, Loader2 } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 import { Button } from './ui/button';
-import { useRouter, usePathname } from 'next/navigation';
 
 const BackButton = ({ className }: { className?: string }) => {
-  const router = useRouter();
   const pathname = usePathname();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleClick = () => {
-    setIsLoading(true);
-    router.back();
-    setIsLoading(false);
-  };
 
   return pathname !== '/' ? (
-    <Button className={`${className}`} onClick={handleClick}>
-      {isLoading ? <Loader2 className="animate-spin" /> : <ChevronLeft />}
-    </Button>
+    <Link href="/" className={`${className}`}>
+      <Button>
+        <ChevronLeft />
+      </Button>
+    </Link>
   ) : null;
 };
 
