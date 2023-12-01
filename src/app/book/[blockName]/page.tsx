80a012018c833ed0303ac587e5e7e6f99f9f9137
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-01 16:18:15 -0600

    make css better for the booking page

--- src/app/book/[blockName]/page.tsx
+++ src/app/book/[blockName]/page.tsx
@@ -18,8 +18,8 @@ export default function bookingPage({
   };
 
   return (
-    <main className="flex flex-col items-center gap-8">
-      <h1 className="mt-10 text-3xl text-center font-bold text-gray-800 dark:text-gray-100">
+    <main className="py-10 flex flex-col items-center gap-8 w-3/4 mx-auto border-2 border-white rounded-md">
+      <h1 className="text-3xl text-center font-bold text-gray-800 dark:text-gray-100">
         Book: {sanitizeBlockName(blockName)}
       </h1>
 
