d2654aeb4c6388726dbe25b58e5e99c0e5ddd230
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-01 16:41:41 -0600

    update supabase and types

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -9,32 +9,67 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      blocks: {
+        Row: {
+          block_name: string
+          created_at: string
+          id: string
+          image_alt: string | null
+          image_url: string | null
+          updated_at: string | null
+        }
+        Insert: {
+          block_name: string
+          created_at?: string
+          id?: string
+          image_alt?: string | null
+          image_url?: string | null
+          updated_at?: string | null
+        }
+        Update: {
+          block_name?: string
+          created_at?: string
+          id?: string
+          image_alt?: string | null
+          image_url?: string | null
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
       bookings: {
         Row: {
+          block_name: string
           check_in_date: string
           check_out_date: string
           created_at: string
           guest_name: string
           id: string
-          room_name: string
         }
         Insert: {
+          block_name: string
           check_in_date: string
           check_out_date: string
           created_at?: string
           guest_name: string
           id?: string
-          room_name: string
         }
         Update: {
+          block_name?: string
           check_in_date?: string
           check_out_date?: string
           created_at?: string
           guest_name?: string
           id?: string
-          room_name?: string
         }
-        Relationships: []
+        Relationships: [
+          {
+            foreignKeyName: "bookings_block_name_fkey"
+            columns: ["block_name"]
+            isOneToOne: false
+            referencedRelation: "blocks"
+            referencedColumns: ["block_name"]
+          }
+        ]
       }
     }
     Views: {
@@ -51,3 +86,83 @@ export interface Database {
     }
   }
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
