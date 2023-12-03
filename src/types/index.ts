22d1cb32122d12157a6c3a50124b62039c5b9544
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-03 13:40:05 -0600

    adds blocks to types def

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,12 @@ export interface Bookings {
   id: string;
   room_name: string;
 }
+
+export interface Blocks {
+  block_name: string;
+  created_at: string;
+  id: string;
+  image_alt: string | null;
+  image_url: string | null;
+  updated_at: string | null;
+}
