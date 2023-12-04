c6be8dbc9c34fcec141344d7277f6f8f2d597876
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-04 13:19:57 -0600

    makes the book now button stick to the bottom

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -37,7 +37,7 @@ const Block = async ({ blockName }: { blockName: string }) => {
 
       <Link
         href={`/book/${blockName.toLowerCase()}`}
-        className="w-full text-center rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        className="mt-auto w-full text-center rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
       >
         Book Now
       </Link>
